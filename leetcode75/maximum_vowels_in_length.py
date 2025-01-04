529c6d5ad591b30e55615dd419dc22d6c189a7a1
Dat Nguyen <datndd260897@gmail.com>
2025-01-03 17:26:07 -0800

    Initialize maximum vowels in length

---
+++ leetcode75/maximum_vowels_in_length.py
@@ -0,0 +1,36 @@
+# Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+# Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+ 
+
+# Example 1:
+
+# Input: s = "abciiidef", k = 3
+# Output: 3
+# Explanation: The substring "iii" contains 3 vowel letters.
+# Example 2:
+
+# Input: s = "aeiou", k = 2
+# Output: 2
+# Explanation: Any substring of length 2 contains 2 vowels.
+# Example 3:
+
+# Input: s = "leetcode", k = 3
+# Output: 2
+# Explanation: "lee", "eet" and "ode" contain 2 vowels.
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def maxVowels(self, s: str, k: int) -> int:
+        max_vowels = 0
+
+        return max_vowels
+
+if __name__ == "__main__":
+    solution = Solution()
+    s, k = "", 3
+    print(solution.maxVowels(s, k))
+    pass    
