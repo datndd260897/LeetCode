89f0726003a023eb63ff8340478d0e4b9d931c78
Dat Nguyen <datndd260897@gmail.com>
2025-01-03 17:13:24 -0800

    [datndd] Slide window method

--- leetcode75/maximum_average_subarray_1.py
+++ leetcode75/maximum_average_subarray_1.py
@@ -42,9 +42,29 @@ class Solution:
             index += 1
             
         return max_average
+    
+    def findMaxAverage_slide_window(self, nums: List[int], k: int) -> float:
+        """_summary_
 
+        Args:
+            nums (List[int]): _description_
+            k (int): _description_
+
+        Returns:
+            float: _description_
+        """
+        current_sum = 0
+        for i in range(k):
+            current_sum += nums[i]
+        max_sum = current_sum
+        for i in range(k, len(nums)):
+            current_sum = current_sum + nums[i] - nums[i - k]
+            max_sum = max(current_sum, max_sum)
+
+        return max_sum/k
 
 if __name__ == "__main__":
     solution = Solution()
     nums, k = [1,12,-5,-6,50,3], 4
     print(solution.findMaxAverage(nums, k))
+    print(solution.findMaxAverage_slide_window(nums, k))
